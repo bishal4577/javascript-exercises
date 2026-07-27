c28e0708a616242d7a77aafa1ba00ea69ade5082
bishal4577 <yadavbishal705@gmail.com>
2026-07-27 09:07:00 +0000

    Build the reverseString Logic

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,21 @@
-const reverseString = function() {
+const reverseString = function(input) {
+
+    if(!input){
+        return "";
+    }
+
+    let arr = [];
+    for(let i=0;i<input.length;i++){
+        arr[i] = input[i];
+    }
+
+    let finalStr = "";
+
+    for(let i = arr.length -1;i>=0;i--){
+        finalStr += arr[i];
+    }
+
+    return finalStr;
 
 };
 
