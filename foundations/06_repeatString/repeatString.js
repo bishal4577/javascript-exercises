9e35ae4f81b2ce330c95406db2af267c246366c3
bishal4577 <yadavbishal705@gmail.com>
2026-07-27 08:38:38 +0000

    Build the easy way of repeatString using loop and array

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num<0) return "ERROR";
+
+    let arr = [];
+    for(let i=0;i<num;i++){
+        arr[i] = string;
+    }
+
+    let finalString = "";
+    for( i of arr){
+        finalString += i;
+    }
+
+return finalString;
 
 };
 
