267fb64040e83277d82fb04db5c151dbc8f224b2
bishal4577 <yadavbishal705@gmail.com>
2026-07-27 10:38:44 +0000

    Build on logic for positiveIntegers or specific dataTypes and complete the exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+function isPositiveInteger(value) {
+    return Number.isInteger(value) && value > 0;
+    }
+
+function swap(num1,num2){
+    let temp = num1;
+    num1 = num2;
+    num2 =temp;
+}
+
+
+
+
+const sumAll = function(start,end) {
+    
+    let sum =0;
+
+    if(isPositiveInteger(start) && isPositiveInteger(end)){
+        if(start > end){
+            swap(start,end);
+            for(let i = end;i<=start;i++){
+                sum += i;
+            }
+        }else{
+            for(let i = start;i<=end;i++){
+                sum += i;
+            }        
+        }
+        
+    }else{
+        return "ERROR";
+    }
+
+return sum;
 
 };
 
