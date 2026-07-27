711edc2156c1bf814524ccee5f104725edec9ecc
bishal4577 <yadavbishal705@gmail.com>
2026-07-27 09:57:30 +0000

    Test and Learn mutliple args handling techniques

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    if(!args) return arr;
+
+    // return arr.filter((arg) => if());
+    
+    let finalArr = [];
+
+    for(let i=0;i<arr.length;i++){
+        if(args.includes(arr[i])){
+        continue;
+        }
+        finalArr.push(arr[i]);
+    }
+
+    // return arr.filter(item => !args.includes(item));
+
+    return finalArr;
+
+
 };
 
 // Do not edit below this line
