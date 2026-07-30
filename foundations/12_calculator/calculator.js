b731c38f9693df96cd7a5f93a90237e90700cd64
bishal4577 <yadavbishal705@gmail.com>
2026-07-30 11:01:11 +0400

    Complete the factorial and exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -26,10 +26,15 @@ const power = function(base, power) {
 	
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
 
+  let result =1;
+    for(let i=1;i<=num;i++){
+      result *= i;
+    }
+
+    return result;
+}
 // Do not edit below this line
 module.exports = {
   add,
