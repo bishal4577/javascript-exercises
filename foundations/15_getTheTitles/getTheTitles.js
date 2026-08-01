29a734dead1e26a59af8c0f117ee78469a296ca4
bishal4577 <yadavbishal705@gmail.com>
2026-08-01 18:52:33 +0400

    Solve the getTitles using books.length and tempArr.push

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,4 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let arr = [];
+    for(let i=0;i<books.length;i++){
+        arr.push(books[i]["title"]);
+    }
+
+    return arr;
 
 };
 
